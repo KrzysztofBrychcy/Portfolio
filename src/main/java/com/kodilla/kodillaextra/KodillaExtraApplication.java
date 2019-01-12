8944a64a096b3cf7f2ccf5676f023c7e21c067ec
KrzysztofBrychcy <brychcy.krzysztof@gmail.com>
2019-01-12 17:48:31 +0100

    Test

---
+++ src/main/java/com/kodilla/kodillaextra/KodillaExtraApplication.java
@@ -0,0 +1,14 @@
+package com.kodilla.kodillaextra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaExtraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaExtraApplication.class, args);
+    }
+
+}
+
